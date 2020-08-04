fd57e7436f027093c276be752cc9f953516848cb
shivashashank <shivashashank.k456@gmail.com>
2020-08-04 13:02:16 +0530

    1

--- Arrays/1. Duplicate Number.py
+++ Arrays/1. Duplicate Number.py
@@ -1,2 +1,45 @@
 # problem link: https://leetcode.com/problems/find-the-duplicate-number/solution/
-# using tortoise method
+# Optimal Solution -> Floyd's Tortoise and Hare algorithm
+
+# Naive approach
+# Time complexity is O(nlogn)
+# Space complexity is O(1)
+
+a = [1,3,4,2,2]
+a.sort()
+for i in range(len(a)-1):
+	if a[i] == a[i+1]:
+		res = a[i]
+print(res)
+
+# using Frequency array/Hashmap
+# Time complexity is O(n)
+# Space complexity is O(n)
+
+a = [1,3,4,2,2]
+freq = {}
+
+for ele in a:
+	if ele in freq:
+		freq[ele] += 1
+	else:
+		freq[ele] = 1
+for key, value in freq.items():
+	if value >= 2:
+		print(key)
+
+# Floyd's Tortoise and Hare algorithm
+# Time complexity is O(n)
+# Space complexity is O(1)
+
+a = [1,3,4,2,2]
+slow = a[0]
+fast = a[a[0]]
+while(slow != fast):
+	slow = a[slow]
+	fast = a[a[fast]]
+fast = 0
+while(slow != fast):
+	slow = a[slow]
+	fast = a[fast]
+print(slow)
